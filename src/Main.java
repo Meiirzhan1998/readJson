5ad3a975dd20fda0225540138fddd4a49411ea63
Meiirzhan1998 <meiirzhan.toktarbayev.kk@gmail.com>
2022-05-05 13:54:00 +0600

    asdf

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,11 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         try {
-            // create object mapper instance
             ObjectMapper mapper = new ObjectMapper();
-
-            // convert JSON file to map
-//            Map<?, ?> map = mapper.readValue(Paths.get("students.json").toFile(), Map.class);
-//            Map<?, ?> map = mapper.readValue(Paths.get("students2.json").toFile(), Map.class);
             Map<?, ?> map = mapper.readValue(Paths.get("students3.json").toFile(), Map.class);
-
-            // print map entries
             for (Map.Entry<?, ?> entry : map.entrySet()) {
                 System.out.println(entry.getKey() + "=" + entry.getValue());
             }
-
         } catch (Exception ex) {
             ex.printStackTrace();
         }
